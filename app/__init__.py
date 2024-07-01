ba9a61cfa14375e62c08a44aeba5057e5319104f
Junction40 <63552059+Junction40@users.noreply.github.com>
2024-07-01 19:51:05 +0100

    Added user login/logout and registration

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__) # Flask application instance
 app.config.from_object(Config)
 db = SQLAlchemy(app) # Initialising the SQLAlchemy extension with the Flask application
 migrate = Migrate(app, db) # Initialising the Flask-Migrate extension with your Flask application and SQLAlchemy instance
+login = LoginManager(app) # Initialising the login manager right after the application instance
+login.login_view = 'login' # 'login' is the fu nction (or endpoint) name for the login vieew
 
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa # General purpose database functions and classes such as types and query building helpers
 import sqlalchemy.orm as so # Support for using models
 from app import db
+from flask_login import UserMixin
 
 # This class inherits from db.Model, a base class for all models from Flask-SQLAlchemy
 # Represent users stored in the database
-class User(db.Model):
+class User(UserMixin, db.Model):
     # Type declaration also makes values required/non-nullable
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
@@ -14,6 +17,14 @@ class User(db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256)) # 'Optional' allows for empty or nullable
     
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author') # This is not an actual database field, but a high-level view of the relationship between users and posts, and for that reason it isn't in the database diagram
+    
+    # Generates a password hash for the current/'self' user's input password
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # Compares the current/'self' user's stored password hash with the input password hashed
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -26,3 +37,9 @@ class Post(db.Model):
     # The __repr__ method tells Python how to print objects of this class, which is going to be useful for debugging
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+# This decorator registers the function as the callback that Flask-Login will use to retrieve the user object based on the user ID stored in the session
+# Automatically loads the user object from the database based on the user ID stored in the session
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
