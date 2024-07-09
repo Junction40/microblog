b7e248681dfe474421560eb6e095f45bc101f368
Junction40 <63552059+Junction40@users.noreply.github.com>
2024-07-09 18:10:01 +0100

    Added error handling & added edit user validation

--- config.py
+++ config.py
@@ -10,3 +10,11 @@ class Config:
     # Tells SQLAlchemy how to connect to the database (e.g., what type of database to use, where it is located).
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    
+    # Email server details for sending errors by email
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
